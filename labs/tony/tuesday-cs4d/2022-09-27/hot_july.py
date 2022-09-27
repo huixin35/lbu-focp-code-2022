404ba999612345b24001ee02d6c0012da6f85538
Tony Jenkins <tony@tony-jenkins.co.uk>
2022-09-27 15:16:40 +0100

    AMJ Tuesday Week 1 code

---
+++ labs/tony/tuesday-cs4d/2022-09-27/hot_july.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+
+    celsius = 38.4
+    fahrenheit = celsius * 1.8 + 32
+
+    print(celsius)
+    print(fahrenheit)
+
+    print(celsius, 'C', ' is ', fahrenheit, 'F', sep='')
+    print(f'{celsius}C is {fahrenheit}F')
