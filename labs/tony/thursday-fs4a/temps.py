0156c00fcc84d1f60d1c88f2712a68ca54219100
Tony Jenkins <tony@tony-jenkins.co.uk>
2022-09-29 10:51:33 +0100

    Tony Thursday lab programs

---
+++ labs/tony/thursday-fs4a/temps.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+
+    celsius = 38.4
+    fahrenheit = celsius * 1.8 + 32
+
+    print(celsius, fahrenheit)
+    print(celsius, 'C is ', fahrenheit, 'F', sep='')
